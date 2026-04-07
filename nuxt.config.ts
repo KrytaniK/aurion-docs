ddd4284e42de8a611e90b2835cadcbaf491cc0c0
Kyle Wick <wick.kyle@gmail.com>
2026-04-07 11:41:50 -0500

    Updated Project Structure & Added Favicon + NuxtContent Dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/content'],
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
+  compatibilityDate: '2026-04-07'
 });
